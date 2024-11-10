e7ed3a03717bd74e9f0f14e7b2fa70a14244de06
TheLukCraft <luk1997r@o2.pl>
2024-11-10 04:08:43 +0100

    Adding projection to the spec part 3

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -22,6 +22,11 @@ public class SpecificationEvaluator<T> where T : BaseEntity
             query = query.OrderByDescending(spec.OrderByDescending);
         }
 
+        if(spec.IsDistinct)
+        {
+            query = query.Distinct();
+        }
+
         return query;
     }
     public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query, ISpecification<T, TResult> spec)
@@ -46,6 +51,11 @@ public class SpecificationEvaluator<T> where T : BaseEntity
         if(spec.Select != null)
         {
             selectQuery = query.Select(spec.Select);
+        }
+
+        if(spec.IsDistinct)
+        {
+            selectQuery = selectQuery?.Distinct();
         }
 
         return selectQuery ?? query.Cast<TResult>();
