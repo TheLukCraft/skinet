6b5898daf1f34ad11050f3df3ea5e43f5cf6b8c8
TheLukCraft <luk1997r@o2.pl>
2024-11-10 02:23:47 +0100

    Adding sorting to the specification

--- Core/Specifications/ProductFilterSpecification.cs
+++
@@ -1,9 +0,0 @@
-using Core.Entities;
-
-namespace Core.Specifications;
-
-public class ProductSpecification(string? brand, string? type) : BaseSpecification<Product>(x => 
-        (string.IsNullOrWhiteSpace(brand) || x.Brand == brand) && 
-        (string.IsNullOrWhiteSpace(type) || x.Type == type))
-{
-}
