c20cdc46bfa6894100f923b257cbf7c2abe410d5
TheLukCraft <luk1997r@o2.pl>
2024-11-11 03:09:49 +0100

    Designing the shop page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,27 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/pagination';
-import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  private shopService = inject(ShopService);
+export class AppComponent{
   title = 'Skinet';
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-    })
-  }
 }
