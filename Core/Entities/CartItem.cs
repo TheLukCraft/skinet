f4a9491ae50a8fd27e0a4d5373a2d7235625f75f
TheLukCraft <luk1997r@o2.pl>
2024-11-12 13:09:32 +0100

    Fixed issues with cart

--- Core/Entities/CartItem.cs
+++ Core/Entities/CartItem.cs
@@ -1,9 +1,9 @@
 namespace Core.Entities;
 
-public class CarItem
+public class CartItem
 {
     public int ProductId { get; set; }
-    public required string Name { get; set; }
+    public required string ProductName { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
     public required string PictureUrl { get; set; }
