5c981a9a45d8bfa2b546335e8e72a3ed99525cc9
TheLukCraft <luk1997r@o2.pl>
2024-11-12 11:40:30 +0100

    Adding a progress bar to indicate loading

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { BusyService } from './../../core/services/busy.service';
+import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatProgressBar } from '@angular/material/progress-bar';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, MatButton, MatBadge, RouterLink, RouterLinkActive],
+  imports: [
+    MatIcon,
+    MatButton,
+    MatBadge,
+    RouterLink,
+    RouterLinkActive,
+    MatProgressBar
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  BusyService = inject(BusyService)
 }
