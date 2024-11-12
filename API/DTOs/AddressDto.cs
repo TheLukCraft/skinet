e6c23ca03286d87a3e48e5b0b36aee28567c0435
TheLukCraft <luk1997r@o2.pl>
2024-11-13 00:16:32 +0100

    Updating the user address part 2

--- API/DTOs/AddressDto.cs
+++ API/DTOs/AddressDto.cs
@@ -6,7 +6,6 @@ public class AddressDto
 {
     [Required]
     public  string Line1 { get; set; } = string.Empty;
-    [Required]
     public string? Line2 { get; set; }
     [Required]
     public  string City { get; set; } = string.Empty;
